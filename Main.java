bc2bb7ca10d5d65f76c8c61c6913d07ef60c0145
juanlopez2605 <juanzubieta26@gmail.com>
2026-03-02 15:10:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,72 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Mascota> lstMascota= new ArrayList<>();
+        Scanner teclado= new Scanner(System.in);
 
-        // PARA QUE EL USUARIO ESCRIBA
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.print("Ingrese el nombre de la mascota: ");
-        String nombre = teclado.nextLine();
-
-        System.out.print("Ingrese la especie: ");
-        String especie = teclado.nextLine();
-
-        System.out.print("Ingrese la edad: ");
-        int edad = teclado.nextInt();
-
-        System.out.print("Ingrese el peso: ");
-        double peso = teclado.nextDouble();
-
-        // Crear objeto con datos ingresados
-        Mascota mascota = new Mascota(nombre, especie, edad, peso);
-
-        int opcion;
+        int op;
 
         do {
-            System.out.println("\n--- MENU CLINICA VETERINARIA ---");
-            System.out.println("1. Mostrar informacion");
-            System.out.println("2. Alimentar mascota");
-            System.out.println("3. Vacunar mascota");
-            System.out.println("4. Hacer cumplir años");
-            System.out.println("5. Mostrar informacion final");
-            System.out.println("0. Salir");
-            System.out.print("Seleccione una opcion: ");
-            opcion = teclado.nextInt();  
+            System.out.println("1. Crear Mascotas ");
+            System.out.println("2. Mostrar Todo ");
+            System.out.println("3. Buscar por nombre ");
+            System.out.println("4. Salir ");
+            op = teclado.nextInt();
 
-            switch (opcion) {
+            switch (op){
                 case 1:
-                    mascota.mostrarInfo();
+                    System.out.println("Crear la mascota");
+                    System.out.println("ingrese la nombre de la mascota ");
+                    String nombre = teclado.next();
+                    System.out.println("ingrese la especie ");
+                    String especie = teclado.next();
+                    System.out.println("ingrese el peso ");
+                    double peso = teclado.nextDouble();
+                    System.out.println("ingrese la edad ");
+                    int edad = teclado.nextInt();
+
+                    Mascota m1 = new Mascota(nombre, especie, edad, peso);
+                    lstMascota.add(m1);
                     break;
 
-                case 2:
-                    System.out.print("Ingrese cantidad de alimento: ");
-                    double cantidad = teclado.nextDouble(); 
-                    mascota.alimentar(cantidad);
-                    break;
+                case 2: for(Mascota m :lstMascota){
+                    System.out.println(m);
+                }
 
                 case 3:
-                    mascota.vacunar();
-                    break;
+                    System.out.println("Buscar mascota");
+                    System.out.println("Ingrese el nombre a buscar");
+                    String nombreBuscado = teclado.next();
 
-                case 4:
-                    mascota.cumplirAnios();
-                    break;
+                    Mascota encontrada = null;
 
-                case 5:
-                    mascota.mostrarInfo();
-                    break;
+                    for (Mascota mascota : lstMascota){
+                        if (mascota.getNombre().equalsIgnoreCase(nombreBuscado)){
+                            encontrada = mascota;
+                            break;
+                        }
+                    }
 
-                case 0:
-                    System.out.println("Saliendo del sistema...");
-                    break;
+                    if (encontrada != null){
+                        System.out.println("Ingrese la accion que desea hacer: ");
+                        System.out.println("1. Vacunar ");
+                        System.out.println("2. Alimentar ");
+                        System.out.println("3. Cumplir años ");
 
-                default:
-                    System.out.println("Opcion invalida.");
+                        int accion = teclado.nextInt();
+
+                        if (accion == 1){
+                            encontrada.vacunar();
+                        } else if (accion == 2){
+                            System.out.println("Ingrese la cantidad de alimento:");
+                            double cantidad = teclado.nextDouble();
+                            encontrada.alimentar(cantidad);
+                        } else if (accion == 3){
+                            encontrada.cumplirAnios();
+                        }
+                    } else {
+                        System.out.println("Mascota no encontrada");
+                    }
+                    break;
             }
+        }while (op !=4);
 
-        } while (opcion != 0);
-
-        teclado.close(); 
     }
 }
